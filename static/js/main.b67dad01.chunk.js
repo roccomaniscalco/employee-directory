52d8d2bd7811bc252a2c4d74c12beb880f928dff
Rocco <roccomaniscalco2001@gmail.com>
2021-01-22 20:45:40 -0500

    Updates

---
+++ static/js/main.b67dad01.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpemployee-directory"]=this["webpackJsonpemployee-directory"]||[]).push([[0],{9:function(e,t,r){"use strict";r.r(t);var o=r(0),c=(r(2),r(3)),n=function(){return Object(o.jsx)("div",{className:"root",children:Object(o.jsx)("h1",{children:"Hello World!"})})};r.n(c).a.render(Object(o.jsx)(n,{}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.b67dad01.chunk.js.map
